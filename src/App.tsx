d5d44269ca777fe20e508a6a07c491762e6f644d
Danielvidarasa442 <163449816+Danielvidarasa442@users.noreply.github.com>
2025-08-28 22:28:03 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css'; 
-import ebookCapa from './assets/ebook-capa.png';
+import ebookCapa from './assets/ebook-capa.jpg';
 
 function App() {
   const [openFaq, setOpenFaq] = useState<number | null>(null);
